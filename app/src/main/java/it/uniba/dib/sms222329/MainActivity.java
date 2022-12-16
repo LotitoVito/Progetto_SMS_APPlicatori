70783db0412791a5ca482aa083254da74678353d
LopezGiuseppe <lopezgiuseppe1996@gmail.com>
2022-12-17 00:16:56 +0100

    #4 implementata una prima versione di pagina di log in, da completare

--- app/src/main/java/it/uniba/dib/sms222329/MainActivity.java
+++ app/src/main/java/it/uniba/dib/sms222329/MainActivity.java
@@ -2,7 +2,9 @@ package it.uniba.dib.sms222329;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Nasconde l'action bar per l'homepage
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        View addButton = findViewById(R.id.signButton);
+        addButton.setOnClickListener(view -> {
+            Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(i);
+        });
+        super.onResume();
     }
 }
