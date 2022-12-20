1de433ff28026c871aaa3d0cff56f484cfcd87ff
giuseppe mele <94959512+peppemele@users.noreply.github.com>
2022-12-20 18:06:24 +0100

    Revert "Revert "#6 Creazione activity e layout per registrazione utente""

---
+++ app/src/main/java/it/uniba/dib/sms222329/SignUp_MainActivity.java
@@ -0,0 +1,6 @@
+package it.uniba.dib.sms222329;
+
+import android.app.Activity;
+
+public class SignUp_MainActivity extends Activity {
+}
---
+++ app/src/main/java/it/uniba/dib/sms222329/Signin_Activity.java
@@ -0,0 +1,6 @@
+package it.uniba.dib.sms222329;
+
+import android.app.Activity;
+
+public class Signin_Activity extends Activity {
+}
