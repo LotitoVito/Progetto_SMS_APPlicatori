7872b3b352d5e472d631b75edd05e59d98ff9e89
giuseppe mele <94959512+peppemele@users.noreply.github.com>
2022-12-20 18:06:15 +0100

    Revert "Revert "#6 Creazione activity e layout per registrazione utente""
    This reverts commit b0240505a520d8a5fa1668d2dec01630ae4ac6af.

---
+++ app/src/main/java/it/uniba/dib/sms222329/activities/SignUp_RelatoreActivity.java
@@ -0,0 +1,16 @@
+package it.uniba.dib.sms222329.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import it.uniba.dib.sms222329.R;
+
+public class SignUp_RelatoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_relatore);
+    }
+}
