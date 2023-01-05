7a098572b914018f2c9ac431836867bc2f7cb4e9
Lopez Giuseppe <lopezgiuseppe1996@gmail.com>
2023-01-05 17:26:20 +0100

    forgot password layout

---
+++ app/src/main/java/it/uniba/dib/sms222329/recuperoPassword.java
@@ -0,0 +1,13 @@
+package it.uniba.dib.sms222329;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class recuperoPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recupero_password);
+    }
+}
