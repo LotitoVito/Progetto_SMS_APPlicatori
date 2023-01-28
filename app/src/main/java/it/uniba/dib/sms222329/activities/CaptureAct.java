d31e7652dc8f1cd0d1aa501ce33ada8174025ef3
LopezGiuseppe <82596234+LopezGiuseppe@users.noreply.github.com>
2023-01-28 19:49:37 +0100

    #21 implementata view scansione codice qr

---
+++ app/src/main/java/it/uniba/dib/sms222329/activities/CaptureAct.java
@@ -0,0 +1,6 @@
+package it.uniba.dib.sms222329.activities;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class CaptureAct extends CaptureActivity {
+}
--- app/src/main/java/it/uniba/dib/sms222329/activities/UtenteLoggato.java
+++ app/src/main/java/it/uniba/dib/sms222329/activities/UtenteLoggato.java
@@ -1,16 +1,20 @@
 package it.uniba.dib.sms222329.activities;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import androidx.appcompat.app.ActionBar;
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
 
 import it.uniba.dib.sms222329.R;
 import it.uniba.dib.sms222329.Utility;
@@ -22,12 +26,10 @@ import it.uniba.dib.sms222329.database.CoRelatoreDatabase;
 import it.uniba.dib.sms222329.database.Database;
 import it.uniba.dib.sms222329.database.RelatoreDatabase;
 import it.uniba.dib.sms222329.database.TesistaDatabase;
-import it.uniba.dib.sms222329.fragment.CameraFragment;
 import it.uniba.dib.sms222329.fragment.ImpostazioniFragment;
 import it.uniba.dib.sms222329.fragment.relatore.HomeFragment;
 import it.uniba.dib.sms222329.fragment.relatore.MessaggiFragment;
 import it.uniba.dib.sms222329.fragment.relatore.ModificaProfiloRelatoreFragment;
-import it.uniba.dib.sms222329.fragment.relatore.ProfiloFragment;
 import it.uniba.dib.sms222329.fragment.relatore.StudentiRelatoreFragment;
 import it.uniba.dib.sms222329.fragment.relatore.TesiFragment;
 
@@ -50,7 +52,7 @@ public class UtenteLoggato extends AppCompatActivity {
             else if (Utente.getTipoUtente() == 2){ //relatore
                 RelatoreLoggato = RelatoreDatabase.IstanziaRelatore(Utente, db);
                 Utility.replaceFragment(getSupportFragmentManager(), R.id.container, new HomeFragment(RelatoreLoggato));
-                setBottomNavigation(new TesiFragment(RelatoreLoggato), new MessaggiFragment(RelatoreLoggato), new HomeFragment(RelatoreLoggato), new StudentiRelatoreFragment(),new CameraFragment());
+                setBottomNavigation(new TesiFragment(RelatoreLoggato), new MessaggiFragment(RelatoreLoggato), new HomeFragment(RelatoreLoggato), new StudentiRelatoreFragment());
             }
             else if (Utente.getTipoUtente() == 3){ //corelatore
                 CoRelatore CoRelatoreLoggato = CoRelatoreDatabase.IstanziaCoRelatore(Utente, db);
@@ -62,7 +64,7 @@ public class UtenteLoggato extends AppCompatActivity {
     }
 
     @SuppressLint("NonConstantResourceId")
-    private void setBottomNavigation(Fragment thesisFragment, Fragment messagesFragment, Fragment homeFragment, Fragment studentFragment, Fragment cameraFragment) {
+    private void setBottomNavigation(Fragment thesisFragment, Fragment messagesFragment, Fragment homeFragment, Fragment studentFragment) {
         BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
 
         // Set a listener for item selection
@@ -82,7 +84,7 @@ public class UtenteLoggato extends AppCompatActivity {
                     Utility.replaceFragment(getSupportFragmentManager(), R.id.container, studentFragment);
                     return true;
                 case R.id.navigation_camera:
-                    Utility.replaceFragment(getSupportFragmentManager(), R.id.container, cameraFragment);
+                    scanQR();
                     return true;
             }
             return false;
@@ -115,4 +117,29 @@ public class UtenteLoggato extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
+
+    private void scanQR(){
+        ScanOptions options = new ScanOptions();
+        options.setPrompt("Volume up to flash on");
+        options.setBeepEnabled(true);
+        options.setOrientationLocked(true);
+        options.setCaptureActivity(CaptureAct.class);
+        barLauncher.launch(options);
+    }
+
+    ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(), result->{
+        if(result.getContents()!=null)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Result");
+            builder.setMessage(result.getContents());
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                }
+            }).show();
+        }
+    });
+
 }
